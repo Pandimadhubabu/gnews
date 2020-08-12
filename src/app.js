7bd4876d6a0ba4be97e7840c8b7601d2bb52df6e
Howard Wang <dr.howardwang@gmail.com>
2020-08-12 04:49:41 -0700

    heroku deploy

--- src/app.js
+++ src/app.js
@@ -1,30 +1,23 @@
 var express = require('express');
 var express = require('express');
+var cron = require('node-cron');
 const routes = require('./routes.js');
-
-// var yup = require('yup');
 
 //mongo setup
 var app = express();
 
 //scheduler
-var cron = require('node-cron');
-
-
+cron.schedule('0 0 */1 * * *', () => {
+  console.log('running a task every second');
+});
 
 app.get('/', (req, res) => res.send('App is working'));
 app.use('/api', routes);
-// cron.schedule('*/1 * * * * *', () => {
-//   console.log('running a task every second');
-// });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
 	console.log(`Listening on ${PORT}`);
 });
-
-
 
 module.exports = {
   app
