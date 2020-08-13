4d382f448f479b32d6bbef72f55247779bb50b5e
Howard Wang <dr.howardwang@gmail.com>
2020-08-13 10:46:50 -0700

    bug fix for now

--- src/scripts/cronjob.js
+++ src/scripts/cronjob.js
@@ -181,7 +181,7 @@ async function handleStaleSearch(db, set) {
 			var input = { type: "search", keyword: term };
 			const data = await fetchNews(input);
 			console.log(`${term} is not stale enough yet...`);
-			if(data) await addArticles(collection, data.articles, false);
+			if(data.articles) await addArticles(collection, data.articles, false);
 			else console.log("DIDN'T WORK FOR", term)
 		}
 	}
